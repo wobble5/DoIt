0793748bba51584d1b5d5fbb245d976d6b185ffb
Trent Stevens <trent.stevens1@gmail.com>
2017-07-23 15:39:59 +1200

    Initial Commit

---
+++ DoIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DoIt
+//
+//  Created by Trent Stevens on 23/07/17.
+//  Copyright © 2017 Trent Stevens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
